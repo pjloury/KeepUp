f8c56c864a60e2c97953e9e4eafcb7bc0ece9b59
PJ <pjloury@gmail.com>
2024-11-28 10:28:09 -0800

    Initial Commit

---
+++ KeepUp/KeepUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepUpApp.swift
+//  KeepUp
+//
+//  Created by PJ Loury on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct KeepUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
